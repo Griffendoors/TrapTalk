ae5a9902a2313b1746d18cc8fa4a4b54f7dfbd44
Ryan Griffin <Ryan Griffin>
2016-08-26 16:40:28 +1200

    first

--- traptalkapp/views.py
+++ traptalkapp/views.py
@@ -3,7 +3,6 @@ from .models import User
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
