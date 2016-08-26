cb991b2f7b3f1601737f919ab0c4e277a9603725
Ryan Griffin <Ryan Griffin>
2016-08-26 16:51:19 +1200

    first

--- traptalkapp/views.py
+++ traptalkapp/views.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 
 
 def index(request):
-  template = loader.get_template('traptalkapp/templates/index.html')
+  template = loader.get_template('index.html')
   return HttpResponse(template.render(request))
 
 
