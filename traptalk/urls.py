83cb81767d3df7c2db543b4fedf9b6f9b98cd6de
Ryan Griffin <Ryan Griffin>
2016-08-26 16:24:19 +1200

    first

--- traptalk/urls.py
+++ traptalk/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import traptalk.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
@@ -11,6 +11,6 @@ import hello.views
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url('signup/$', views.signup, name='signup'),
     url(r'^admin/', include(admin.site.urls)),
 ]
