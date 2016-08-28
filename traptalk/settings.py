05c3ba1049c13836a336f479a1f7270d44058bbb
Ryan Griffin <Ryan Griffin>
2016-08-28 20:31:36 +1200

    first

--- traptalk/settings.py
+++ traptalk/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'traptalkapp'
+    'traptalkapp',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,25 +76,24 @@ WSGI_APPLICATION = 'traptalk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-
 #DATABASES = {
  #   'default': {
-  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   #     'NAME': 'traptalk',
-    #    'USER': 'postgres',
-     #   'PASSWORD': 'matthew1',
-      #  'HOST': 'localhost',
-       # 'PORT': '5432',
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #}
 #}
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ddrq6c8kh9d8bu',
+        'USER': 'ehrhnrbwkypipl',
+        'PASSWORD': 'r-qSsSOfDTXHIbrmnddJyBvkEt',
+        'HOST': 'postgres://ehrhnrbwkypipl:r-qSsSOfDTXHIbrmnddJyBvkEt@ec2-23-23-199-72.compute-1.amazonaws.com:5432/ddrq6c8kh9d8bu',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
