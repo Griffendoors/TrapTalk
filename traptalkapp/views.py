3d00c000e533ad41f1e941fba2fdd50d10714887
Ryan Griffin <Ryan Griffin>
2016-09-01 14:37:11 +1200

    errors

--- traptalkapp/views.py
+++ traptalkapp/views.py
@@ -74,7 +74,8 @@ def signin(request):
 
 
 def main(request):
-  return redirect('templates/traptalk/main.html');
+  template = loader.get_template('traptalk/main.html')
+  return HttpResponse(template.render(request))
 
 
 
