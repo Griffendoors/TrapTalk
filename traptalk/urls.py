e832a6c54188a7aaf8a5da549a8367d7f3eac78c
Ryan Griffin <Ryan Griffin>
2016-09-01 14:21:06 +1200

    errors

--- traptalk/urls.py
+++ traptalk/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     url(r'^$', traptalkapp.views.index, name='index'),
     url('signup$', traptalkapp.views.signup, name='signup'),
     url('signin$', traptalkapp.views.signin, name='signin'),
-    url('main$', traptalkapp.views.main, name='main'),
+    #url('main$', traptalkapp.views.main, name='main'),
     url(r'^admin/', include(admin.site.urls)),
 ]
