9f0d7532f857ab3293a8ec2ed29e6c9bb4cc7d02
Ryan Griffin <Ryan Griffin>
2016-09-04 15:39:53 +1200

    enter key press to send message

--- traptalkapp/views.py
+++ traptalkapp/views.py
@@ -54,7 +54,7 @@ def signin(request):
     u = User.objects.get(username__exact = username)
 
     if(u.password != password):
-      response = JsonResponse({'status':'false','message': 'Username or Password incorrect'}, status=403)
+      response = JsonResponse({'status':'false','message': 'Username or Password incorrect', 'username': username}, status=403)
       return response
 
 
