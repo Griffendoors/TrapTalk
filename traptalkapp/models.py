7217c270a37858a88c7214faadafd984677247ab
Ryan Griffin <Ryan Griffin>
2016-09-05 14:30:22 +1200

    added tokens, friends, message schemas

--- traptalkapp/models.py
+++ traptalkapp/models.py
@@ -16,12 +16,12 @@ class ValidToken(models.Model):
 	issued = models.DateTimeField(auto_now_add=True, blank=True)
 
 class Friend(models.Model):
-	friend_one = models.ForeignKey('User', related_name = 'one', on_delete = models.CASCADE)
-	friend_two = models.ForeignKey('User', related_name = 'two',  on_delete = models.CASCADE)
+	friend_one = models.ForeignKey('User', related_name = 'adder', on_delete = models.CASCADE)
+	friend_two = models.ForeignKey('User', related_name = 'addee',  on_delete = models.CASCADE)
 	since = models.DateTimeField(auto_now_add=True, blank=True)
 
 class Message(models.Model):
-	message_from = models.ForeignKey('User',  related_name = 'one', on_delete = models.CASCADE)
-	message_to = models.ForeignKey('User', related_name = 'two',  on_delete = models.CASCADE)
+	message_from = models.ForeignKey('User',  related_name = 'sender', on_delete = models.CASCADE)
+	message_to = models.ForeignKey('User', related_name = 'sendee',  on_delete = models.CASCADE)
 	sent = models.DateTimeField(auto_now_add=True, blank=True)
 	message_contents = models.CharField(max_length = 1000)
