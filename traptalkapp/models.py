8b8fc7a1ad54b4e97b23344f97ae760f64da94a4
Ryan Griffin <Ryan Griffin>
2016-09-05 15:28:23 +1200

    added tokens, friends, message schemashb

--- traptalkapp/models.py
+++ traptalkapp/models.py
@@ -4,29 +4,25 @@ from django.db import models
 class User(models.Model):
 	username = models.CharField(max_length=15)
 	password = models.CharField(max_length=200)
-	class Meta:
-        managed = True
-
 
 
 class ValidToken(models.Model):
 	token = models.CharField(max_length = 50)
 	validFor = models.ForeignKey('User', on_delete=models.CASCADE)
 	issued = models.DateTimeField(auto_now_add=True, blank=True)
-	class Meta:
-        managed = True
+
 
 class Friend(models.Model):
 	friend_one = models.ForeignKey('User', related_name = 'adder', on_delete = models.CASCADE)
 	friend_two = models.ForeignKey('User', related_name = 'addee',  on_delete = models.CASCADE)
 	since = models.DateTimeField(auto_now_add=True, blank=True)
-	class Meta:
-        managed = True
+
 
 class Message(models.Model):
 	message_from = models.ForeignKey('User',  related_name = 'sender', on_delete = models.CASCADE)
 	message_to = models.ForeignKey('User', related_name = 'sendee',  on_delete = models.CASCADE)
 	sent = models.DateTimeField(auto_now_add=True, blank=True)
 	message_contents = models.CharField(max_length = 1000)
-	class Meta:
+	
+class Meta:
         managed = True	
