cbbc209e3d18a21c3612d08b65313f6ff516ad3c
Ryan Griffin <Ryan Griffin>
2016-09-27 20:29:04 +1300

    cleaning up

--- traptalk/urls.py
+++ traptalk/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url('signup$', traptalkapp.views.signup, name='signup'),
     url('signin$', traptalkapp.views.signin, name='signin'),
     url('signout$', traptalkapp.views.signout, name='signout'),
-    url('addFriend$', traptalkapp.views.signout, name='addFriend'),
+    url('addFriend$', traptalkapp.views.addFriend, name='addFriend'),
     url('main$', traptalkapp.views.main, name='main'),
     url(r'^admin/', include(admin.site.urls)),
 ]
