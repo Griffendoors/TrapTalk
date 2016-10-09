e57ce406e9fd56a622bee11ef951389f4ffe32df
Ryan Griffin <Ryan Griffin>
2016-10-09 17:13:21 +1300

    parsing new messages as with api call

--- traptalkapp/views.py
+++ traptalkapp/views.py
@@ -198,8 +198,8 @@ def getParticularMessages(request):
   if User.objects.filter(username__exact = selected).exists():
     s = User.objects.get(username__exact = selected)
 
-
-  messages = (Message.objects.values(message_from=u).order_by('sent') & Message.objects.values(message_to=s).order_by('sent'))| (Message.objects.values(message_to=u).order_by('sent') & Message.objects.values(message_from=s).order_by('sent'))
+  #messages = (Message.objects.values(message_from=u).order_by('sent') & Message.objects.values(message_to=s).order_by('sent'))| (Message.objects.values(message_to=u).order_by('sent') & Message.objects.values(message_from=s).order_by('sent'))
+  messages = (Message.objects.values('message_from', 'message_to', 'message_contents').order_by('sent'))
   #response = JsonResponse(dict(genres=list(Genre.objects.values('name', 'color'))))
 
   return JsonResponse(messages)
